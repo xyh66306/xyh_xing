26a972b32b57235f025c9a6d4434af6188d75674
提莫队长 <=>
2025-11-15 10:05:38 +0800

    1115

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -1,4 +1,4 @@
 <?php
 //000000000000
  exit();?>
-a:4:{s:11:"config_init";a:1:{i:1;s:29:"\addons\summernote\Summernote";}s:8:"sms_send";a:0:{}s:10:"sms_notice";a:0:{}s:9:"sms_check";a:0:{}}
+a:4:{s:11:"config_init";a:2:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";i:1;s:29:"\addons\summernote\Summernote";}s:8:"sms_send";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}s:10:"sms_notice";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}s:9:"sms_check";a:1:{i:0;s:27:"\addons\qcloudsms\Qcloudsms";}}
