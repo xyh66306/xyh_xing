5e9190c21d623e491b4734a962fa82fb75a2aa53
提莫队长 <=>
2026-01-29 14:11:03 +0800

    Update Notice.php

--- application/job/Notice.php
+++ application/job/Notice.php
@@ -3,9 +3,8 @@
  * @Author: 提莫队长 =
  * @Date: 2025-11-10 17:01:45
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2026-01-29 14:08:56
+ * @LastEditTime: 2026-01-29 14:10:22
  * @FilePath: \xyh_xing\application\job\Notice.php
-//  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 namespace app\job;
